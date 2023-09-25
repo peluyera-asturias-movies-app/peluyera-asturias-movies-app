fcb69d9988206ceeaece427f25a69abcf2679b05
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-09-25 12:02:39 -0500

    edit & delete buttons fixed

--- js/index.js
+++ js/index.js
@@ -1,177 +1,61 @@
-import { getMovies, getMovieById, getMovieByTitleOMDB, deleteMovie,postMovie,searchMovieByTitleLocal,patchMovie,searchMoviesTMDB,latestMoviesList,showLoader,hideLoader} from "../js/utils/movies.js"
-
+import { getMovies, getMovieById, getMovieByTitleOMDB, deleteMovie,postMovie,searchMovieByTitleLocal,patchMovie,searchMoviesTMDB,latestMoviesList,showLoader,hideLoader} from "./utils/movies.js"
+import {renderModal, renderMovie, renderCategories, renderPoster} from "./renderFunctions.js";
 
 //////// MAIN METHOD
 (async () => {
-
-
-
-
-
-
-
-// THIS IS FOR ADD A MOVIE BUTTON FORM
-// Get the modal and button elements
-const addMovieModal = document.getElementById("addMovieModal");
-//button to open the add movie form
-const addMovieButton = document.getElementById("addMovieButton");
-//event to Show the modal when the button is clicked
-addMovieButton.addEventListener("click", () => {
-addMovieModal.style.display = "block";
-});
-//button to close modal form
-const closeModal = document.getElementById("closeModal");
-// Close the modal when the close button is clicked
-//event to close the add movie form
-closeModal.addEventListener("click", () => {
-    addMovieModal.style.display = "none";
-});
-
-// Close the modal when clicking outside the modal
-window.addEventListener("click", (event) => {
-    if (event.target == addMovieModal) {
-        addMovieModal.style.display = "none";
-    }
-});
-
-//////////////////////////////// ADD MOVIE
-
-//variable to capture value of add movie form
-let newMovieObj;
-
-// below 4 varialbes capture values from add movie form
-let addMovieForm = document.getElementById("movie-form")
-let addMovieFormTitle = document.getElementById("title").value;
-let addMovieFormrating = document.getElementById("rating").value;
-
-
-
-//button to submit the add movie form
-let addMovieBtn = document.getElementById("add-movie-btn");
-// event to add movie to favorites when btn clicked
-addMovieBtn.addEventListener("click", (e)=>{
-    e.preventDefault()
-
-    newMovieObj = {
-        title: addMovieForm[0].value,
-        vote_average: Number(addMovieForm[1].value)
-    };
-    console.log(newMovieObj);
-    postMovie(newMovieObj);
-
-
-});
-//////////////////////////////////////////////////
-
-
-
-
-
-
-//button to submit the search movie
-// let searchMovieBtn = document.getElementById("search-movie-btn");
-
-
-
-
-
-//////////////////////////// // EDIT MOVIE FORM
-
-// Function to display the edit modal with movie details
-// Get the modal and button elements
-const editMovieModal = document.getElementById("editMovieModal");
-const editMovieButton = document.getElementById("editMovieButton");
-const closeModalTwo = document.getElementById("closeModal-two");
-
-// Show the modal when the button is clicked
-editMovieButton.addEventListener("click", () => {
-    editMovieModal.style.display = "block";
-});
-
-// Close the modal when the close button is clicked
-closeModalTwo.addEventListener("click", () => {
-    editMovieModal.style.display = "none";
-});
-
-// Close the modal when clicking outside the modal
-window.addEventListener("click", (event) => {
-    if (event.target == editMovieModal) {
-        editMovieModal.style.display = "none";
-    }
-});
-/////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
     // variable for preloader
     let loader = document.getElementById("preloader")
+    // THIS IS FOR ADD A MOVIE BUTTON FORM
+    // Get the modal and button elements
+    const addMovieModal = document.getElementById("addMovieModal");
+    //button to open the add movie form
+    const addMovieButton = document.getElementById("addMovieButton");
+    //button to close modal form
+    const closeModal = document.getElementById("closeModal");
+    //variable to capture value of add movie form
+    let newMovieObj;
+    // below 4 varialbes capture values from add movie form
+    const addMovieForm = document.getElementById("movie-form")
+    let addMovieFormTitle = document.getElementById("title").value;
+    let addMovieFormrating = document.getElementById("rating").value;
+    //button to submit the add movie form
+    let addMovieBtn = document.getElementById("add-movie-btn");
+
+    //event to Show the modal when the button is clicked
+    addMovieButton.addEventListener("click", () => {
+    addMovieModal.style.display = "block";
+    });
+
+    // Close the modal when the close button is clicked
+    //event to close the add movie form
+    closeModal.addEventListener("click", () => {
+        addMovieModal.style.display = "none";
+    });
+
+    // Close the modal when clicking outside the modal
+    window.addEventListener("click", (event) => {
+        if (event.target == addMovieModal) {
+            addMovieModal.style.display = "none";
+        }
+    });
+    //////////////////////////////// ADD MOVIE
+
+    // event to add movie to favorites when btn clicked
+    addMovieBtn.addEventListener("click", (e)=>{
+        e.preventDefault()
+
+        newMovieObj = {
+            title: addMovieForm[0].value,
+            vote_average: Number(addMovieForm[1].value)
+        };
+        console.log(newMovieObj);
+        postMovie(newMovieObj);
 
 
-    // window.addEventListener("load",preLoader)
+    });
+    //////////////////////////////////////////////////
 
-
-
-    // function creates and adds moive card to DOM
-    const renderMovie = (movie, target) => {
-        const movieCard = document.createElement("article");
-        movieCard.classList.add("movie-card");
-        movieCard.innerHTML = `
-            <div class="movie-card-title">${movie.title}</div>
-            <p class="movie-card-year">${movie.release_date}</p>
-            <div class="d-flex, full-width, heigth align-items center">
-            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path ?? "NO IMAGE FOUND" }" class="poster-img" alt="poster-img">
-            </div>
-            ${movie.overview && `<p class="movie-card-description">${movie.overview}.</p>`}
-            <div class="d-flex align-items-center justify-content-between">
-                <span class="movie-card-span">Rating</span>
-                <span class="movie-card-rating">${movie.vote_average.toFixed()}/10</span>
-            </div>
-            <meter class="movie-card-meter" min="0" max="10" value="${movie.vote_average}"></meter>
-            <div class="d-flex align-items-center justify-content-start gap-10 flex-wrap">
-                ${movie.categories && renderCategories(movie.categories)}
-            </div>
-            <input type="hidden" value="${movie.id}">
-           
-            <button  class="delete-movie-btn">Delete Movie</button>
-        `;
-        // IF we had buttons in here that needed event listeners, we would do it here
-        // const editBtn = bookCard.querySelector("button");
-        // editBtn.addEventListener("click", async () => {
-        //     /// DO THE THANG!
-        // });
-        // THEN append it into the DOM
-        target.appendChild(movieCard);
-    };
-    // function assings category based on data
-    const renderCategories = (categories = []) => {
-        // create a single HTML string made up of all the categories
-        const categoriesHTML = categories?.map((category) => `<span class="movie-card-tag">${category}</span>`).join("");
-        return categoriesHTML;
-    };
-
-
-    // function to display movie posters on carousel
-    const renderPoster = (movie, target) => {
-        const posterCard = document.createElement("div");
-        posterCard.classList.add("carousel-item");
-        posterCard.innerHTML = `
-
-           <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" class="poster-img" alt="poster-img">
-            
-        `;
-
-        target.appendChild(posterCard);
-    };
-
-
-/////////////////////ON LOAD///////////////////
+    /////////////////////ON LOAD///////////////////
     //// displays card on load
     const movies = await getMovies();
     console.log(movies);
@@ -206,31 +90,6 @@ window.addEventListener("click", (event) => {
         });
 
     }
-
-
-    ///////////////////////////// EDIT MOVIE
-    let editMovieForm = document.getElementById("edit-movie-form");
-
-    let editMovieFormBtn = document.getElementById("edit-form-submit-btn");
-
-    let editMovieObj;
-
-    editMovieFormBtn.addEventListener("click", (e)=>{
-        e.preventDefault();
-
-        editMovieObj = {
-            id: editMovieForm[0].value,
-            title: editMovieForm[1].value,
-            release_date: editMovieForm[2].value,
-            overview: editMovieForm[3].value
-        }
-        console.log(editMovieObj);
-        console.log(e);
-        //FUNCTION TO PATCH MOVIE GOES HERE
-        patchMovie(e.target.previousElementSibling.value);
-
-    });
-
 
 
 
---
+++ js/renderFunctions.js
@@ -0,0 +1,108 @@
+import { getMovies, getMovieById, getMovieByTitleOMDB, deleteMovie,postMovie,searchMovieByTitleLocal,patchMovie,searchMoviesTMDB,latestMoviesList,showLoader,hideLoader} from "./utils/movies.js"
+
+// function creates and adds moive card to DOM
+export const renderModal = (movie) => {
+    const modal = document.createElement('div');
+    modal.classList.add('custom-modal');
+    modal.innerHTML = `
+        <div class="custom-modal-bg"></div>
+        <div class="custom-modal-content">
+            <span class="close" id="closeModal-two" data-bs-dismiss="modal">&times;</span>
+            <h2>Edit ${movie.title}</h2>
+            <form id="edit-movie-form">
+                <label for="description-edit-form">Description:</label>
+                <textarea type="text" id="description-edit-form">${movie.overview}</textarea>
+                <button id="edit-form-submit-btn" type="submit">Update</button>
+            </form>
+        </div>
+    `;
+    const submitBtn = modal.querySelector('#edit-form-submit-btn');
+    const closeBtn = modal.querySelector('.close');
+    const modalBG = modal.querySelector('.custom-modal-bg');
+
+    submitBtn.addEventListener('click', async ()=>{
+        const newDescription = modal.querySelector("#description-edit-form");
+        const newDescValue = newDescription.value;
+        const movieObj = {
+            id: movie.id,
+            overview: newDescValue
+        }
+        try {
+            await patchMovie(movieObj);
+            modal.remove();
+        } catch(error) {
+            alert(error);
+        }
+    });
+    closeBtn.addEventListener('click', ()=>{
+       modal.remove();
+    });
+    modalBG.addEventListener('click', ()=>{
+        modal.remove();
+    });
+    document.body.appendChild(modal);
+}
+export const renderMovie = (movie, target) => {
+    const movieCard = document.createElement("article");
+    movieCard.classList.add("movie-card");
+    movieCard.innerHTML = `
+            <div class="movie-card-title">${movie.title}</div>
+            <p class="movie-card-year">${movie.release_date}</p>
+            <div class="d-flex, full-width, heigth align-items center">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path ?? "NO IMAGE FOUND" }" class="poster-img" alt="poster-img">
+            </div>
+            ${movie.overview && `<p class="movie-card-description">${movie.overview}.</p>`}
+            <div class="d-flex align-items-center justify-content-between">
+                <span class="movie-card-span">Rating</span>
+                <span class="movie-card-rating">${movie.vote_average.toFixed()}/10</span>
+            </div>
+            <meter class="movie-card-meter" min="0" max="10" value="${movie.vote_average}"></meter>
+            <div class="d-flex align-items-center justify-content-start gap-10 flex-wrap">
+                ${movie.categories && renderCategories(movie.categories)}
+            </div>
+            <input type="hidden" value="${movie.id}">
+            <button  class="edit-movie-btn" data-bs-toggle="modal" data-bs-target="#editModal${movie.id}">Edit Movie</button>
+            <button  class="delete-movie-btn">Delete Movie</button>
+        `;
+    // IF we had buttons in here that needed event listeners, we would do it here
+    const editBtn = movieCard.querySelector('.edit-movie-btn');
+    editBtn.addEventListener('click', ()=>{
+        renderModal(movie);
+    });
+    const deleteBtn = movieCard.querySelector('.delete-movie-btn');
+    deleteBtn.addEventListener('click', async ()=>{
+        try {
+            await deleteMovie(movie.id);
+            movieCard.remove();
+        } catch (e){
+            console.log(e);
+        }
+    });
+    // const editBtn = bookCard.querySelector("button");
+    // editBtn.addEventListener("click", async () => {
+    //     /// DO THE THANG!
+    // });
+    // THEN append it into the DOM
+    target.appendChild(movieCard);
+};
+
+// function assings category based on data
+export const renderCategories = (categories = []) => {
+    // create a single HTML string made up of all the categories
+    const categoriesHTML = categories?.map((category) => `<span class="movie-card-tag">${category}</span>`).join("");
+    return categoriesHTML;
+};
+
+// function to display movie posters on carousel
+export const renderPoster = (movie, target) => {
+    const posterCard = document.createElement("div");
+    posterCard.classList.add("carousel-item");
+    posterCard.innerHTML = `
+
+           <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" class="poster-img" alt="poster-img">
+            
+        `;
+
+    target.appendChild(posterCard);
+};
+
